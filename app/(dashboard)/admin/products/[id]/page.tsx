bbdcd9256f3ab56329b71125307214d999e1aad9
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-04-11 17:20:26 +0200

    admin dashboard update product completed

--- app/(dashboard)/admin/products/[id]/page.tsx
+++ app/(dashboard)/admin/products/[id]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { CustomButton, DashboardSidebar } from "@/components";
+import { CustomButton, DashboardSidebar, SectionTitle } from "@/components";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 interface DashboardProductDetailsProps {
   params: { id: number };
@@ -11,6 +12,20 @@ const DashboardProductDetails = ({
   params: { id },
 }: DashboardProductDetailsProps) => {
   const [product, setProduct] = useState<any>();
+  
+
+  const updateProduct = async () => {
+    console.log(product);
+    
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product),
+    };
+    fetch(`http://localhost:3001/api/products/${id}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => toast.success("Product successfully updated"));
+  };
 
   const uploadFile = async (file: any) => {
     const formData = new FormData();
@@ -46,7 +61,7 @@ const DashboardProductDetails = ({
   return (
     <div className="bg-white flex justify-start max-w-screen-2xl mx-auto h-[100vh]">
       <DashboardSidebar />
-      <div className="flex flex-col gap-y-7 ml-5">
+      <div className="flex flex-col gap-y-7 ml-5 w-full">
         <div>
           <label className="form-control w-full max-w-xs">
             <div className="label">
@@ -72,7 +87,7 @@ const DashboardProductDetails = ({
               className="input input-bordered w-full max-w-xs"
               value={product?.price}
               onChange={(e) =>
-                setProduct({ ...product, price: e.target.value })
+                setProduct({ ...product, price: Number(e.target.value) })
               }
             />
           </label>
@@ -97,7 +112,7 @@ const DashboardProductDetails = ({
             <div className="label">
               <span className="label-text">Is product in stock?</span>
             </div>
-            <select className="select select-bordered" value={product?.inStock}>
+            <select className="select select-bordered" value={product?.inStock} onChange={(e) => setProduct({...product, inStock: Number(e.target.value)})}>
               <option value={1}>Yes</option>
               <option value={0}>No</option>
             </select>
@@ -112,19 +127,39 @@ const DashboardProductDetails = ({
               setProduct({ ...product, mainImage: e.target.files[0].name });
             }}
           />
+          {product?.mainImage && <Image
+            src={`/` + product?.mainImage}
+            alt={product?.title}
+            className="w-auto h-auto"
+            width={100}
+            height={100}
+          />}
+        </div>
+        <div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Product description:</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              value={product?.description}
+              onChange={(e) =>
+                setProduct({ ...product, description: e.target.value })
+              }
+            ></textarea>
+          </label>
         </div>
         <div className="flex gap-x-2">
-          <CustomButton
-            paddingX={5}
-            paddingY={5}
-            text="Update product"
-            buttonType="button"
-            customWidth="100px"
-            textSize="lg"
-          />
           <button
             type="button"
-            className="w=[100px] uppercase bg-red-600 px-10 py-5 text-lg border border-black border-gray-300 font-bold text-white shadow-sm hover:bg-red-700 hover:text-white focus:outline-none focus:ring-2"
+            onClick={updateProduct}
+            className="uppercase bg-blue-600 px-10 py-5 text-lg border border-black border-gray-300 font-bold text-white shadow-sm hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-2"
+          >
+            Update product
+          </button>
+          <button
+            type="button"
+            className="uppercase bg-red-600 px-10 py-5 text-lg border border-black border-gray-300 font-bold text-white shadow-sm hover:bg-red-700 hover:text-white focus:outline-none focus:ring-2"
           >
             Delete product
           </button>
--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,302 +1,329 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function getAllProducts(request, response) {
-    const dividerLocation = request.url.indexOf("?");
-    let filterObj = {};
-    let sortObj = {};
-    let sortByValue = "defaultSort";
-    if (dividerLocation !== -1) {
-        // queryArray sada sadrzi ceo query deo sto ukljucuje filter i sort queryije
-        const queryArray = request.url
-            .substring(dividerLocation + 1, request.url.length)
-            .split("&");
+  const dividerLocation = request.url.indexOf("?");
+  let filterObj = {};
+  let sortObj = {};
+  let sortByValue = "defaultSort";
+  if (dividerLocation !== -1) {
+    // queryArray sada sadrzi ceo query deo sto ukljucuje filter i sort queryije
+    const queryArray = request.url
+      .substring(dividerLocation + 1, request.url.length)
+      .split("&");
 
-        let filterType;
-        let filterArray = [];
+    let filterType;
+    let filterArray = [];
 
-        for (let i = 0; i < queryArray.length; i++) {
-            // proveravam da li je u pitanju filter mod i price filter
-            if (
-                queryArray[i].indexOf("filters") !== -1 &&
-                queryArray[i].indexOf("price") !== -1
-            ) {
-                // uzimam "price" deo. Naravno mogao sam samo da napisem filterType="price"
-                filterType = queryArray[i].substring(
-                    queryArray[i].indexOf("price"),
-                    queryArray[i].indexOf("price") + "price".length
-                );
-            }
-            // proveravam da li je u pitanju filter mod i rating filter
-            if (
-                queryArray[i].indexOf("filters") !== -1 &&
-                queryArray[i].indexOf("rating") !== -1
-            ) {
-                // uzimam "rating" deo. Naravno mogao sam samo da napisem filterType="rating"
-                filterType = queryArray[i].substring(
-                    queryArray[i].indexOf("rating"),
-                    queryArray[i].indexOf("rating") + "rating".length
-                );
-            }
+    for (let i = 0; i < queryArray.length; i++) {
+      // proveravam da li je u pitanju filter mod i price filter
+      if (
+        queryArray[i].indexOf("filters") !== -1 &&
+        queryArray[i].indexOf("price") !== -1
+      ) {
+        // uzimam "price" deo. Naravno mogao sam samo da napisem filterType="price"
+        filterType = queryArray[i].substring(
+          queryArray[i].indexOf("price"),
+          queryArray[i].indexOf("price") + "price".length
+        );
+      }
+      // proveravam da li je u pitanju filter mod i rating filter
+      if (
+        queryArray[i].indexOf("filters") !== -1 &&
+        queryArray[i].indexOf("rating") !== -1
+      ) {
+        // uzimam "rating" deo. Naravno mogao sam samo da napisem filterType="rating"
+        filterType = queryArray[i].substring(
+          queryArray[i].indexOf("rating"),
+          queryArray[i].indexOf("rating") + "rating".length
+        );
+      }
 
-            // proveravam da li je u pitanju filter mod i category filter
-            if (
-                queryArray[i].indexOf("filters") !== -1 &&
-                queryArray[i].indexOf("category") !== -1
-            ) {
-                // uzimam "category" deo. Naravno mogao sam samo da napisem filterType="rating"
-                filterType = queryArray[i].substring(
-                    queryArray[i].indexOf("category"),
-                    queryArray[i].indexOf("category") + "category".length
-                );
-            }
+      // proveravam da li je u pitanju filter mod i category filter
+      if (
+        queryArray[i].indexOf("filters") !== -1 &&
+        queryArray[i].indexOf("category") !== -1
+      ) {
+        // uzimam "category" deo. Naravno mogao sam samo da napisem filterType="rating"
+        filterType = queryArray[i].substring(
+          queryArray[i].indexOf("category"),
+          queryArray[i].indexOf("category") + "category".length
+        );
+      }
 
-            if (
-                queryArray[i].indexOf("filters") !== -1 &&
-                queryArray[i].indexOf("inStock") !== -1
-            ) {
-                // uzimam "rating" deo. Naravno mogao sam samo da napisem filterType="rating"
-                filterType = queryArray[i].substring(
-                    queryArray[i].indexOf("inStock"),
-                    queryArray[i].indexOf("inStock") + "inStock".length
-                );
-            }
+      if (
+        queryArray[i].indexOf("filters") !== -1 &&
+        queryArray[i].indexOf("inStock") !== -1
+      ) {
+        // uzimam "rating" deo. Naravno mogao sam samo da napisem filterType="rating"
+        filterType = queryArray[i].substring(
+          queryArray[i].indexOf("inStock"),
+          queryArray[i].indexOf("inStock") + "inStock".length
+        );
+      }
 
-            if (
-                queryArray[i].indexOf("filters") !== -1 &&
-                queryArray[i].indexOf("outOfStock") !== -1
-            ) {
-                // uzimam "rating" deo. Naravno mogao sam samo da napisem filterType="rating"
-                filterType = queryArray[i].substring(
-                    queryArray[i].indexOf("outOfStock"),
-                    queryArray[i].indexOf("outOfStock") + "outOfStock".length
-                );
-            }
+      if (
+        queryArray[i].indexOf("filters") !== -1 &&
+        queryArray[i].indexOf("outOfStock") !== -1
+      ) {
+        // uzimam "rating" deo. Naravno mogao sam samo da napisem filterType="rating"
+        filterType = queryArray[i].substring(
+          queryArray[i].indexOf("outOfStock"),
+          queryArray[i].indexOf("outOfStock") + "outOfStock".length
+        );
+      }
 
-            if (queryArray[i].indexOf("sort") !== -1) {
-                // uzimamo vrednost sorta iz querija
-                sortByValue = queryArray[i].substring(queryArray[i].indexOf("=") + 1);
-            }
+      if (queryArray[i].indexOf("sort") !== -1) {
+        // uzimamo vrednost sorta iz querija
+        sortByValue = queryArray[i].substring(queryArray[i].indexOf("=") + 1);
+      }
 
-            // proveravam da li je u datom queriju filters mod
-            if (queryArray[i].indexOf("filters") !== -1) {
-                let filterValue;
-                //  proveravam da nije filter po categoriji. To radim kako bih izbegao pretvaranje Stringa u Int
-                if (queryArray[i].indexOf("category") === -1) {
-                    // uzimam value deo. To je deo gde se nalazi int vrednost querija i konvertujem je u broj jer je po defaultu string
-                    filterValue = parseInt(
-                        queryArray[i].substring(
-                            queryArray[i].indexOf("=") + 1,
-                            queryArray[i].length
-                        )
-                    );
-                } else {
-                    // ako je filter po kategorije
-                    filterValue = queryArray[i].substring(
-                        queryArray[i].indexOf("=") + 1,
-                        queryArray[i].length
-                    );
-                }
+      // proveravam da li je u datom queriju filters mod
+      if (queryArray[i].indexOf("filters") !== -1) {
+        let filterValue;
+        //  proveravam da nije filter po categoriji. To radim kako bih izbegao pretvaranje Stringa u Int
+        if (queryArray[i].indexOf("category") === -1) {
+          // uzimam value deo. To je deo gde se nalazi int vrednost querija i konvertujem je u broj jer je po defaultu string
+          filterValue = parseInt(
+            queryArray[i].substring(
+              queryArray[i].indexOf("=") + 1,
+              queryArray[i].length
+            )
+          );
+        } else {
+          // ako je filter po kategorije
+          filterValue = queryArray[i].substring(
+            queryArray[i].indexOf("=") + 1,
+            queryArray[i].length
+          );
+        }
 
-                // uzimam operator npr. lte, gte, gt, lt...
-                const filterOperator = queryArray[i].substring(
-                    queryArray[i].indexOf("$") + 1,
-                    queryArray[i].indexOf("=") - 1
-                );
+        // uzimam operator npr. lte, gte, gt, lt...
+        const filterOperator = queryArray[i].substring(
+          queryArray[i].indexOf("$") + 1,
+          queryArray[i].indexOf("=") - 1
+        );
 
-                // sve to dodajemo u filterArray
-                // primer izgleda filterArray:
-                /*
+        // sve to dodajemo u filterArray
+        // primer izgleda filterArray:
+        /*
                 [
                 { filterType: 'price', filterOperator: 'lte', filterValue: 3000 },
                 { filterType: 'rating', filterOperator: 'gte', filterValue: 0 }
                 ]
                 */
-                filterArray.push({ filterType, filterOperator, filterValue });
-            }
-        }
-
-        for (let item in filterArray) {
-            filterObj = {
-                ...filterObj,
-                [filterArray[item].filterType + ""]: {
-                    [filterArray[item].filterOperator]: filterArray[item].filterValue,
-                },
-            };
-        }
+        filterArray.push({ filterType, filterOperator, filterValue });
+      }
     }
 
-    if (sortByValue === "defaultSort") {
-        sortObj = [];
-    } else if (sortByValue === "titleAsc") {
-        sortObj = [
-            {
-                title: "asc",
-            },
-        ];
-    } else if (sortByValue === "titleDesc") {
-        sortObj = [
-            {
-                title: "desc",
-            },
-        ];
-    } else if (sortByValue === "lowPrice") {
-        sortObj = [
-            {
-                price: "asc",
-            },
-        ];
-    } else if (sortByValue === "highPrice") {
-        sortObj = [
-            {
-                price: "desc",
-            },
-        ];
+    for (let item in filterArray) {
+      filterObj = {
+        ...filterObj,
+        [filterArray[item].filterType + ""]: {
+          [filterArray[item].filterOperator]: filterArray[item].filterValue,
+        },
+      };
     }
+  }
 
-    let users;
-    if (Object.keys(filterObj).length === 0) {
-        users = await prisma.product.findMany({});
-    } else {
-        users = await prisma.product.findMany < object > ({
-            where: filterObj,
-            orderBy: sortObj,
-        });
-    }
-    return response.json(users);
+  if (sortByValue === "defaultSort") {
+    sortObj = [];
+  } else if (sortByValue === "titleAsc") {
+    sortObj = [
+      {
+        title: "asc",
+      },
+    ];
+  } else if (sortByValue === "titleDesc") {
+    sortObj = [
+      {
+        title: "desc",
+      },
+    ];
+  } else if (sortByValue === "lowPrice") {
+    sortObj = [
+      {
+        price: "asc",
+      },
+    ];
+  } else if (sortByValue === "highPrice") {
+    sortObj = [
+      {
+        price: "desc",
+      },
+    ];
+  }
+
+  let users;
+  if (Object.keys(filterObj).length === 0) {
+    users = await prisma.product.findMany({});
+  } else {
+    users =
+      (await prisma.product.findMany) <
+      object >
+      {
+        where: filterObj,
+        orderBy: sortObj,
+      };
+  }
+  return response.json(users);
 }
 
 async function createProduct(request, response) {
-    try {
-        console.log("Creating product:", request.body);
-        const { slug, title, mainImage, price, rating, description, manufacturer, category, inStock } = request.body;
-        const product = await prisma.product.create({
-            data: {
-                
-                slug,
-                title,
-                mainImage,
-                price,
-                rating,
-                description,
-                manufacturer,
-                category,
-                inStock
-                
-            }
-        });
-        console.log("Product created:", product); // Dodajemo log za proveru
-        return response.status(201).json(product);
-    } catch (error) {
-        console.error("Error creating product:", error); // Dodajemo log za proveru
-        return response.status(500).json({ error: "Error creating product" });
-    }
+  try {
+    console.log("Creating product:", request.body);
+    const {
+      slug,
+      title,
+      mainImage,
+      price,
+      rating,
+      description,
+      manufacturer,
+      category,
+      inStock,
+    } = request.body;
+    const product = await prisma.product.create({
+      data: {
+        slug,
+        title,
+        mainImage,
+        price,
+        rating,
+        description,
+        manufacturer,
+        category,
+        inStock,
+      },
+    });
+    console.log("Product created:", product); // Dodajemo log za proveru
+    return response.status(201).json(product);
+  } catch (error) {
+    console.error("Error creating product:", error); // Dodajemo log za proveru
+    return response.status(500).json({ error: "Error creating product" });
+  }
 }
 
 // Metoda za ažuriranje postojećeg proizvoda
 async function updateProduct(request, response) {
-    try {
-        const { id } = request.params; // Dobijamo slug iz params
-        console.log("Updating product with id:", id); // Dodajemo log za proveru
-        const { title, mainImage, price, rating, description, manufacturer, category, inStock } = request.body;
-        
-        // Pronalazimo proizvod prema slug-u
-        const existingProduct = await prisma.product.findUnique({
-            where: {
-                id
-            }
-        });
+  try {
+    const { id } = request.params; // Dobijamo slug iz params
+    const {
+      slug,
+      title,
+      mainImage,
+      price,
+      rating,
+      description,
+      manufacturer,
+      category,
+      inStock,
+    } = request.body;
+    // Pronalazimo proizvod prema slug-u
+    const existingProduct = await prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
 
-        if (!existingProduct) {
-            return response.status(404).json({ error: "Product not found" });
-        }
-
-        // Ažuriramo pronađeni proizvod
-        const updatedProduct = await prisma.product.update({
-            where: {
-                id: existingProduct.id // Koristimo ID pronađenog proizvoda
-            },
-            data: {
-                title,
-                mainImage,
-                price,
-                rating,
-                description,
-                manufacturer,
-                category,
-                inStock
-            }
-        });
-
-        return response.json(updatedProduct);
-    } catch (error) {
-        return response.status(500).json({ error: "Error updating product" });
+    if (!existingProduct) {
+      return response.status(404).json({ error: "Product not found" });
     }
+
+    
+    console.log(id, title, mainImage, price, rating, description, manufacturer, category, inStock);
+
+    // Ažuriramo pronađeni proizvod
+    const updatedProduct = await prisma.product.update({
+      where: {
+        id // Koristimo ID pronađenog proizvoda
+      },
+      data: {
+        title: title,
+        mainImage: mainImage,
+        price: price,
+        rating: rating,
+        description: description,
+        manufacturer: manufacturer,
+        category: category,
+        inStock: inStock,
+      },
+    });
+
+    return response.json(updatedProduct);
+  } catch (error) {
+    return response.status(500).json({ error: "Error updating product" });
+  }
 }
 
 // Metoda za brisanje proizvoda
 async function deleteProduct(request, response) {
-    try {
-        const { id } = request.params;
-        console.log("Deleting product with slug:", id); // Dodajemo log za proveru
-        await prisma.product.delete({
-            where: {
-                id
-            }
-        });
-        return response.status(204).send();
-    } catch (error) {
-        return response.status(500).json({ error: "Error deleting product" });
-    }
+  try {
+    const { id } = request.params;
+    console.log("Deleting product with slug:", id); // Dodajemo log za proveru
+    await prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+    return response.status(204).send();
+  } catch (error) {
+    return response.status(500).json({ error: "Error deleting product" });
+  }
 }
 
 async function searchProducts(request, response) {
-    try {
-        const { query } = request.query;
-        if (!query) {
-            return response.status(400).json({ error: "Query parameter is required" });
-        }
-
-        const products = await prisma.product.findMany({
-            where: {
-                OR: [
-                    {
-                        title: {
-                            contains: query
-                        }
-                    },
-                    {
-                        description: {
-                            contains: query
-                        }
-                    }
-                ]
-            }
-        });
-
-        return response.json(products);
-    } catch (error) {
-        console.error("Error searching products:", error);
-        return response.status(500).json({ error: "Error searching products" });
+  try {
+    const { query } = request.query;
+    if (!query) {
+      return response
+        .status(400)
+        .json({ error: "Query parameter is required" });
     }
+
+    const products = await prisma.product.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: query,
+            },
+          },
+          {
+            description: {
+              contains: query,
+            },
+          },
+        ],
+      },
+    });
+
+    return response.json(products);
+  } catch (error) {
+    console.error("Error searching products:", error);
+    return response.status(500).json({ error: "Error searching products" });
+  }
 }
 
 async function getProductById(request, response) {
-    const { id } = request.params;
-    const product = await prisma.product.findUnique({
-        where: {
-          id: id
-        }
-      });
-    console.log(product);
-    if (!product) {
-        return response.status(404).json({ error: "Product not found" });
-    }
-    return response.status(200).json(product)
+  const { id } = request.params;
+  const product = await prisma.product.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  console.log(product);
+  if (!product) {
+    return response.status(404).json({ error: "Product not found" });
+  }
+  return response.status(200).json(product);
 }
 
-
-
-module.exports = { getAllProducts, createProduct, updateProduct, deleteProduct, searchProducts, getProductById };
-
-
+module.exports = {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  searchProducts,
+  getProductById,
+};
