08d1ee67d184c38cf59a8b2a42220767acd5020f
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2025-11-07 15:53:03 +0100

    added temporary return in header until wishlist is fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -49,6 +49,8 @@ const Header = () => {
       slug:string
       stockAvailabillity: number;
     }[] = [];
+
+    return; // temporary disable wishlist fetching while the issue is being resolved
     
     wishlist.map((item: any) => productArray.push({id: item?.product?.id, title: item?.product?.title, price: item?.product?.price, image: item?.product?.mainImage, slug: item?.product?.slug, stockAvailabillity: item?.product?.inStock}));
     
